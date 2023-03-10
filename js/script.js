54f545162e1a490cf0f38d8d41719787dbc48adb
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-03-10 20:56:42 +0300

    fix: gitignore modifyed

--- js/script.js
+++ js/script.js
@@ -10,8 +10,7 @@ form.addEventListener("submit", function (event) {
   let date = form.date.value;
   let text = form.comment.value;
 
-  // валидация и обработка ошибок
-
+  // валидация и обработка ошибо
   if (name.length < 3) {
     nameContainerEl.classList.add("error");
     return;
@@ -20,20 +19,36 @@ form.addEventListener("submit", function (event) {
     textContainerEl.classList.add("error");
     return;
   }
+
+  // Обработка даты
   if (!date) {
     date = new Date();
+  } else {
+    date = new Date(date);
   }
+  let day = getMessageDateTime(date); // Сегодня, Вчера, 20.11.2012
 
+  let time = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  let dateTime = `${day}, ${time}`; // склеиваем День + Время
+
+  //Создаем новый комментарий и добавляем его в HTML
+  let newComment = createComment(name, dateTime, text);
+  commentsBody.append(newComment);
+
+  // очищаем форму
   form.name.value = "";
   form.date.value = "";
   form.comment.value = "";
-
-  let newComment = createComment(name, date, text);
-  commentsBody.append(newComment);
 });
+
 form.name.addEventListener("input", function (e) {
   nameContainerEl.classList.remove("error");
 });
+
 form.comment.addEventListener("input", function (e) {
   textContainerEl.classList.remove("error");
 });
@@ -81,4 +96,37 @@ function createComment(name, date, text) {
   });
 
   return comment;
+}
+
+function dropHMS(date) {
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0, 0);
+}
+
+function getMessageDateTime(dateTime) {
+  let today = new Date(), // присвоение и форматированние текущей даты
+    yesterday = new Date(), // присвоение и форматирование текущей даты - 1 день
+    roomLastMessageDate = new Date(dateTime); // присвоение и форматирование даты последнего сообщения комнаты
+
+  yesterday.setDate(today.getDate() - 1);
+
+  dropHMS(today);
+  dropHMS(yesterday);
+  dropHMS(roomLastMessageDate);
+
+  if (dateTime) {
+    if (today.getTime() === roomLastMessageDate.getTime()) {
+      return "Сегодня";
+    } else if (yesterday.getTime() === roomLastMessageDate.getTime()) {
+      return "Вчера";
+    } else {
+      const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      };
+      return roomLastMessageDate.toLocaleString("ru-RU", options);
+    }
+  }
 }
